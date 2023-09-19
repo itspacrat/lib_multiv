99d63097b5cc12397cde4d9fa253621285fcca2f
itspacrat <blakemichaelgaynor@gmail.com>
2023-09-19 13:11:09 -0700

    add player instantiation and loading fns `new` and `from_existing`

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use {
     serde::{Deserialize, Serialize},
     serde_json::{from_str, to_string, to_string_pretty},
     std::{
-        cell::RefCell,
+        //cell::RefCell,
         collections::HashMap,
         path::{Path, PathBuf},
     },
@@ -20,31 +20,10 @@ use {
     //shvft_mapper::parsemap
 };
 
-/// Position type for the turtle (should be usize)
+/// Position type for the player (should be usize)
 pub type Pos = usize;
-pub type TurtAttrs = Vec<TurtAttr>;
-#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, FromRepr)]
-#[repr(u8)]
-pub enum TileDatum {
-    NullTile = 0,
-    FloorTile = 1,
-    WallTile = 2,
-    WorkBenchTile = 3,
-    ClosedDoorTile = 4,
-    OpenDoorTile = 5,
-    WoodCrateTile = 6,
-    CardboardBoxTile = 7,
-    NoteTile = 8,
-    ComputerTile = 9,
-    ScannerUpgrade = 10,
-    ScannerIIUpgrade = 11,
-    LockedDoorTile = 12,
-    LockedIIDoorTile = 13,
-    SwitchTile = 14,
-    EndpointTile = 15,
-}
 #[derive(PartialEq, Deserialize, Serialize, Clone, Debug)]
-pub enum TurtAttr {
+pub enum MvTileAttribute {
     Null,
     NoPassThrough,
     Push,
@@ -59,211 +38,40 @@ pub enum TurtAttr {
     Write,
     State,
 }
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftContainer {
-    pub pos: Pos,
-    pub inventory: Vec<TileDatum>,
-}
-/// requires attribute "opendoor"
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftNote {
-    pub pos: Pos,
-    pub content: String,
-}
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftDoor {
-    pub here: Pos,
-    pub there: Pos,
-    pub exit_map: String,
-    pub exit_direction: char,
-}
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftSwitch {
-    pub state: bool,
-    pub map: String,
-    pub here: Pos,
-    pub there: Pos,
-}
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftMap {
-    pub width: Pos,
-    pub tiles: Vec<TileDatum>,
-    pub doors: Vec<ShvftDoor>,
-    pub notes: Vec<ShvftNote>,
-    pub containers: Vec<ShvftContainer>,
-}
-pub type ShvftRgb = [u8; 3];
+pub type MvTileAttributes = Vec<MvTileAttribute>;
+
+pub type MvRGB = [u8; 3];
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct DbItem {
     pub description: String,
-    pub rgb: ShvftRgb,
-    pub attributes: TurtAttrs,
+    pub rgb: MvRGB,
+    pub attributes: MvTileAttributes,
 }
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftInfo {
-    pub current_map: String,
-    pub nametag: String,
-    pub db: HashMap<String, DbItem>,
-    pub pos: Pos,
-    pub inventory: Vec<TileDatum>,
-    pub rail: Vec<u8>,
-}
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct ShvftTurtle {
-    pub domain: String,
-    pub info: ShvftInfo,
-    pub map: ShvftMap,
-}
-impl ShvftTurtle {
-    pub async fn mv(&mut self, dirs: Vec<char>) {
-        let Self {
-            info: refinfo,
-            map: refmap,
-            ..
-        } = self;
-        let mut mv_out: Pos;
-        for d in dirs.iter() {
-            let attrs_next = get_attrs(
-                &refinfo.db,
-                refmap.tiles[next_pos(*d, &refinfo.pos, &refmap)],
-            )
-            .unwrap();
+// todo: necessary??? idk
+pub type DB = HashMap<String,DbItem>;
 
-            if attrs_next.contains(&TurtAttr::NoPassThrough) {
-                if attrs_next.contains(&TurtAttr::Push) {
-                    let next = next_pos(*d, &refinfo.pos, refmap);
-                    let mut dummy: TileDatum = TileDatum::from_repr(0).unwrap();
-
-                    // swap push tiles
-                    dummy = refmap.tiles[refinfo.pos];
-                    refmap.tiles[refinfo.pos] = refmap.tiles[next];
-                    refmap.tiles[next] = dummy;
-
-                    mv_out = next_pos(*d, &(refinfo.pos), &refmap)
-                } else {
-                    mv_out = refinfo.pos;
-                }
-            } else {
-                mv_out = next_pos(*d, &(refinfo.pos), &refmap);
-            }
-
-            refinfo.pos = mv_out;
-        }
-        let _ = write(
-            format!(
-                "domains/{}/maps/{}/map.json",
-                self.domain, self.info.current_map
-            ),
-            to_string_pretty(&refmap).unwrap(),
-        )
-        .await
-        .unwrap();
-    }
-
-    pub fn container_swap(&mut self, box_dir: char, give_index: usize, take_index: usize) {
-        
-
-        let Self {
-            info: ShvftInfo { pos: refpos, .. },
-            map: refmap,
-            ..
-        } = self;
-        let mut refcontainers = refmap.containers.clone();
-        let np = next_pos(box_dir, &refpos.clone(), refmap);
-
-        let container_ref = get_container_mut(
-            &mut refcontainers,
-            refpos,
-            &np,
-        )
-        .context("no container found :(")
-        .unwrap();
-
-        std::mem::swap(
-            &mut container_ref.inventory[take_index],
-            &mut self.info.inventory[give_index],
-        );
-        ()
-    }
-}
-//
-/// returns a mutable reference to a `ShvftContainer` via an index
-fn get_container_mut<'a>(
-    containers: &'a mut Vec<ShvftContainer>,
-    pos: &'a Pos,
-    destination_pos: &'a Pos,
-) -> Option<&'a mut ShvftContainer> {
-
-    let mut count: usize = 0;
-    let mut out_idx: usize = 0;
-
-    for (i, c) in containers.iter_mut().enumerate() {
-        if c.pos == *destination_pos {
-            count += 1;
-            out_idx = i;
-        } else {
-        }
-    }
-
-    if count == 1 {
-        Some(&mut containers[out_idx])
-    } else {
-        None
-    }
-}
-fn get_attrs(db: &HashMap<String, DbItem>, item_id: TileDatum) -> Result<&[TurtAttr]> {
+fn get_attrs(db: &HashMap<String, DbItem>, item_id: u8) -> Result<&[MvTileAttribute]> {
     Ok(&db
         .get(&format!("{:?}", item_id))
         .context("NOT_VALID_ITEM")?
         .attributes)
 }
-//
-/// creates a new instance of a turtle from default values at domain
-pub async fn new(domain: String) -> ShvftTurtle {
-    let current_map = read_to_string(format!("domains/{}/info/current_map.txt", &domain))
-        .await
-        .unwrap();
-    let map: ShvftMap = from_str(
-        &read_to_string(&format!(
-            "domains/{}/maps/{}/map.json",
-            domain, &current_map
-        ))
-        .await
-        .unwrap(),
-    )
-    .unwrap();
 
-    ShvftTurtle {
-        domain: domain.clone(),
-        info: ShvftInfo {
-            current_map: current_map.to_owned(),
-            nametag: String::from("turtle"),
-            db: from_str(&read_to_string("domains/global/item_db.json").await.unwrap()).unwrap(),
-            pos: from_str(
-                &read_to_string(format!("domains/{}/info/pos.json", &domain))
-                    .await
-                    .unwrap(),
-            )
-            .unwrap(),
-            inventory: from_str(
-                &read_to_string(format!("domains/{}/info/inv.json", &domain))
-                    .await
-                    .unwrap(),
-            )
-            .unwrap(),
-            rail: from_str(
-                &read_to_string(format!("domains/{}/info/rail.json", &domain))
-                    .await
-                    .unwrap(),
-            )
-            .unwrap(),
-        },
-        map: map.to_owned(),
-    }
+/// ### MvRoom
+/// 
+/// the room typestruct has a designated list of access keys, 
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MvRoom {
+    keys: [Vec<String>;2],
+    id: String,
+    tiles: Vec<u8>,
+    width: usize
 }
-/// sets & returns the potential map index given a direction and a ShvftMap reference
-pub fn next_pos(dir: char, c_pos: &Pos, data: &ShvftMap) -> Pos {
-    let height = ((data.width as f32 / data.tiles.len() as f32).ceil()) as usize;
+//
+/// creates a new instance of a turtle from default values at key
+/// sets & returns the potential map index given a direction and an MvRoom reference
+pub fn next_pos(dir: char, c_pos: &Pos, data: &MvRoom) -> Pos {
+    //let height = ((data.width as f32 / data.tiles.len() as f32).ceil()) as usize;
     let potential_pos: Pos;
     match dir {
         'n' | 'N' => {
@@ -300,6 +108,91 @@ pub fn next_pos(dir: char, c_pos: &Pos, data: &ShvftMap) -> Pos {
     }
     potential_pos
 }
+/// ### MvPlayer
+#[derive(PartialEq, Deserialize, Serialize, Clone, Debug)]
+pub struct MvPlayer {
+    keys: Vec<String>,
+    /// a string denoting the file name (without the file extension) 
+    /// of the room the player is currently in
+    room_id: String,
+    /// a player's position on the cuttent map
+    position: Pos,
+    /// the inventory stores items a player has taken
+    inventory: Vec<u8>,
+    /// the rail holds up to 3 of a player's equippable items
+    rail: Vec<u8>,
+}
+
+impl MvPlayer {
+    /// should be called after checking whether or not a map exists for a given player key.
+    /// 
+    /// instantiates a new player with a given player key
+    pub fn new(default_key:String) -> Self {
+        Self {
+            keys: vec![default_key.clone()],
+            room_id: default_key.clone(),
+            position: 24,
+            inventory:vec![0,0,0,0,0,0,0],
+            rail: vec![0,0,0]
+        }
+    }
+    //
+    /// loads playerdata from a file matching the current player's key
+    pub async fn from_existing(default_key: String) -> MvPlayer {
+        
+        let player: MvPlayer =  from_str(
+            &read_to_string(
+                format!("players/{}/data.json",default_key.clone())
+            ).await.unwrap()
+        ).unwrap();
+        
+        player
+    }
+    //
+    /// handles moving the palyer in a room
+    pub async fn mv(&mut self, room: &mut MvRoom, db: &DB, dirs: Vec<char>) {
+        let mut mv_out: Pos;
+        for d in dirs.iter() {
+            let attrs_next = get_attrs(
+                db,
+                room.tiles[next_pos(*d, &self.position,room)],
+            )
+            .unwrap();
+
+            if attrs_next.contains(&MvTileAttribute::NoPassThrough) {
+                if attrs_next.contains(&MvTileAttribute::Push) {
+                    let next = next_pos(*d, &self.position, room);
+                    let mut dummy: u8 = 0_u8;
+
+                    // swap push tiles
+                    dummy = room.tiles[self.position];
+                    room.tiles[self.position] = room.tiles[next];
+                    room.tiles[next] = dummy;
+
+                    mv_out = next_pos(*d, &(self.position), room)
+                } else {
+                    mv_out = self.position;
+                }
+            } else {
+                mv_out = next_pos(*d, &(self.position), &room);
+            }
+
+            self.position = mv_out;
+        }
+        let _ = write(
+            format!(
+                "keys/{}/maps/{}/data.json",
+                self.keys[0], self.room_id
+            ),
+            to_string_pretty(room).unwrap(),
+        )
+        .await
+        .unwrap();
+    }
+}
+// blake put his blood sweat and tears into this, do not give 
+// it the disrespect of commenting it out
+/// returns the index of the first free slot in the player's inventory
 pub fn ck_inv_empty(inventory: &mut Vec<u8>) -> Option<usize> {
     let out: Option<usize>;
     let mut free_slots: Vec<usize> = Vec::new();
@@ -319,3 +212,130 @@ pub fn ck_inv_empty(inventory: &mut Vec<u8>) -> Option<usize> {
     }
     out
 }
+
+/* 
+#[derive(Deserialize, Serialize, Debug, Clone, Copy, PartialEq, FromRepr)]
+#[repr(u8)]
+pub enum MvTile {
+    NullTile = 0,
+    FloorTile = 1,
+    WallTile = 2,
+    WorkBenchTile = 3,
+    ClosedDoorTile = 4,
+    OpenDoorTile = 5,
+    WoodCrateTile = 6,
+    CardboardBoxTile = 7,
+    NoteTile = 8,
+    ComputerTile = 9,
+    ScannerUpgrade = 10,
+    ScannerIIUpgrade = 11,
+    LockedDoorTile = 12,
+    LockedIIDoorTile = 13,
+    SwitchTile = 14,
+    EndpointTile = 15,
+}
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MvBox {
+    pub pos: Pos,
+    pub inventory: Vec<u8>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MvNote {
+    pub pos: Pos,
+    pub content: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MvDoor {
+    pub here: Pos,
+    pub there: Pos,
+    pub exit_map: String,
+    pub exit_direction: char,
+}
+
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MvPlayerInfo {
+    pub current_map: String,
+    pub nametag: String,
+    pub db: HashMap<String, DbItem>,
+    pub pos: Pos,
+    pub inventory: Vec<u8>,
+    pub rail: Vec<u8>,
+}
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MvPlayer {
+    pub key: String,
+    pub info: MvPlayerInfo,
+    pub map: MvRoom,
+}
+impl MvPlayer {
+
+    pub fn container_swap(&mut self, box_dir: char, give_index: usize, take_index: usize) {
+        
+
+        let Self {
+            info: MvPlayerInfo { pos: refpos, .. },
+            map: refmap,
+            ..
+        } = self;
+        let mut refcontainers = refmap.containers.clone();
+        let np = next_pos(box_dir, &refpos.clone(), refmap);
+
+        let container_ref = get_container_mut(
+            &mut refcontainers,
+            refpos,
+            &np,
+        )
+        .context("no container found :(")
+        .unwrap();
+
+        std::mem::swap(
+            &mut container_ref.inventory[take_index],
+            &mut self.info.inventory[give_index],
+        );
+
+        let _ = write(
+            format!(
+                "keys/{}/maps/{}/map.json",
+                self.key, self.info.current_map
+            ),
+            to_string_pretty(&refmap).unwrap(),
+        );
+        let _ = write(
+            format!(
+                "keys/{}/info/inv.json",
+                self.key
+            ),
+            to_string_pretty(&self.info.inventory).unwrap(),
+        );
+
+    }
+}
+//
+/// returns a mutable reference to a `ShvftContainer` via an index
+fn get_container_mut<'a>(
+    containers: &'a mut Vec<ShvftContainer>,
+    pos: &'a Pos,
+    destination_pos: &'a Pos,
+) -> Option<&'a mut ShvftContainer> {
+
+    let mut count: usize = 0;
+    let mut out_idx: usize = 0;
+
+    for (i, c) in containers.iter_mut().enumerate() {
+        if c.pos == *destination_pos {
+            count += 1;
+            out_idx = i;
+        } else {
+        }
+    }
+
+    if count == 1 {
+        Some(&mut containers[out_idx])
+    } else {
+        None
+    }
+}
+*/
